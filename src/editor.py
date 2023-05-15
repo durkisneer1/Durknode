e454e715d25275b3f1cdb33e365c2aa8c4ccd95f
cool-dev-guy <dev.guy.cool@gmail.com>
2023-05-15 21:56:44 +0530

    wow cool edits

--- src/editor.py
+++ src/editor.py
@@ -2,7 +2,8 @@ import pygame as pg
 from src.nodes.add import AddNode
 from src.nodes.number import NumberNode
 
-
+CANVASX = 0
+CANVASY = 0
 class NodeEditor:
     def __init__(self, font):
         self.nodes = []
@@ -27,14 +28,16 @@ class NodeEditor:
             node.update(events, mouse_pos)
             for ev in events:
                 if ev.type == pg.MOUSEBUTTONDOWN and ev.button == 1:
-                    if node.rect.collidepoint(ev.pos):
+                    if node.bar_rect.collidepoint(ev.pos):
                         node.set_mouse_offset(ev.pos)
                         node.dragging = True
                         [n.__setattr__("selected", False) for n in self.nodes]
                         node.selected = True
+                        self.nodes[self.nodes.index(node)],self.nodes[len(self.nodes)-1] = self.nodes[len(self.nodes)-1],self.nodes[self.nodes.index(node)]
                         break
                 elif ev.type == pg.MOUSEBUTTONUP and ev.button == 1:
                     node.dragging = False
+                    node.zindex = 0
                     break
 
     def draw(self, screen):
